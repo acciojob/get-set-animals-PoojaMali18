08cf0c54635ab891c4683b68ffb89760921e21ca
PoojaMali18 <141479524+PoojaMali18@users.noreply.github.com>
2023-11-21 12:53:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor (species)
+	{
+		this.species=species;
+	}
+	get species()
+	{
+		return this.species;
+	}
+	makesound()
+	{
+      console.log("Sshhhhh");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	purr()
+	{
+		console.log("purr");
+		
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	bark()
+	{
+		console.log("woof");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
